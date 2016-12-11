efe024b4cecd488b19f9a8939ab0a1b5cc260d1b
alaasdk <alaasdk@gmail.com>
2016-12-11 23:40:07 +0200

    add basic implementation with 1 generic unit test case used to generate code.

---
+++ NVS.Common.Infrastructure.Test/BroadcasterTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NVS.Common.Infrastructure.Messaging;
+
+namespace NVS.Common.Infrastructure.Test
+{
+    [TestClass]
+    public class BroadcasterTest
+    {
+        [TestMethod]
+        public void Broadcaster_basicUsage()
+        {
+            var HandlerCallingCount = 0;
+
+            Broadcaster bc = new Broadcaster();
+
+            // Send message, nobody will recieve it
+            bc.Publish(new Message(payload: "content", tag: "mytag"));
+
+            // Subscribe
+            string token = bc.Subscribe("mytag", (message) => { HandlerCallingCount++; });
+
+            // Send another one
+            bc.Publish(new Message(payload: "content_msg2", tag: "mytag"));
+
+            // Assert.
+            Assert.AreEqual<int>(HandlerCallingCount, 1);
+
+            // Unsubscribe.
+            bc.Unsubscribe(token);
+
+            // Send another one, nobody will recieve it.
+            bc.Publish(new Message(payload: "content_msg3", tag: "mytag"));
+
+            // Assert.
+            Assert.AreEqual<int>(HandlerCallingCount, 1);
+        }
+    }
+}
---
+++ NVS.Common.Infrastructure/Messaging/Broadcaster.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVS.Common.Infrastructure.Messaging
+{
+    public class Broadcaster
+    {
+        private class TagSubscriber
+        {
+            public string token { get; set; }
+            public Action<Message> handler { get; set; }
+        }
+
+        Dictionary<string, List<TagSubscriber>> _tagsSubscribers;
+
+        public Broadcaster()
+        {
+            _tagsSubscribers = new Dictionary<string, List<TagSubscriber>>();
+        }
+
+
+        public string Subscribe(string tag, Action<Message> handler)
+        {
+            if (!_tagsSubscribers.ContainsKey(tag))
+            {
+                AddTag(tag);
+            }
+
+                
+            // generate token
+            var token = Guid.NewGuid().ToString();
+
+            _tagsSubscribers[tag].Add(new TagSubscriber()
+            {
+                token = token,
+                handler = handler
+            });
+
+            return token;
+        }
+
+        
+        public bool Unsubscribe(string token)
+        {
+            foreach (var tagSubscribers in _tagsSubscribers)
+            {
+                foreach (var subscriber in tagSubscribers.Value)
+                {
+                    if (subscriber.token == token)
+                    {
+                        tagSubscribers.Value.Remove(subscriber);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+
+        public void Publish(Message message)
+        {
+            if (!_tagsSubscribers.ContainsKey(message.Tag))
+            {
+                AddTag(message.Tag);
+                return;
+            }
+
+            Broadcast(message);
+        }
+
+
+
+        private void Broadcast(Message message)
+        {
+            _tagsSubscribers[message.Tag].ForEach((subscriber) =>
+            {
+                subscriber.handler(message);
+            });
+        }
+
+        private void AddTag(string tag)
+        {
+            _tagsSubscribers.Add(tag, new List<TagSubscriber>());
+        }
+
+
+        //TODO: ideas
+        // * implement a queue to save the messages to the broadcaster locally and handle fails.
+        // * single instance of broadcaster (singleton )
+    }
+}
